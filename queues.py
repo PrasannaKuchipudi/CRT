1be5988c528b3ca79f1c8d117a2b320e589fa837
PrasannaKuchipudi <prasannakuchipudi99@gmail.com>
2025-09-13 19:08:39 +0530

    Initial commit

--- queues.py
+++ queues.py
@@ -1,25 +1,61 @@
-#12-09-2025
-#Implementation using List
+# #12-09-2025
+# #Implementation using List
+# class Queue:
+#     def __init__(self):
+#         self.queue=[]
+#     def enqueue(self,data):
+#         self.queue.append(data)
+#     def isEmpty(self):
+#         return len(self.queue)==0
+#     def dequeue(self):
+#         if self.isEmpty():
+#             print("Queue is empty")
+#             return 
+#         return self.queue.pop(0) 
+#     def peek(self):
+#         if self.isEmpty():
+#             print("No elements in queue")
+#             return
+#         return self.queue[0]
+# a=Queue()
+# a.enqueue(1)
+# a.enqueue(2)
+# a.enqueue(3)
+# a.dequeue()
+# print(a.peek())   
+#Implementation using Linked List 
+class Node:
+    def __init__(self,data):
+        self.data=data 
+        self.next=None 
 class Queue:
     def __init__(self):
-        self.queue=[]
+        self.head=None 
+        self.tail=None 
     def enqueue(self,data):
-        self.queue.append(data)
+        newNode=Node(data)
+        if self.head==None:
+            self.head=newNode 
+            self.tail=newNode
+        else:
+            self.tail.next=newNode 
+            self.tail=newNode 
     def isEmpty(self):
-        return len(self.queue)==0
+        return self.head==None 
     def dequeue(self):
         if self.isEmpty():
             print("Queue is empty")
-            return 
-        return self.queue.pop(0) 
+        else:
+            self.head=self.head.next 
     def peek(self):
         if self.isEmpty():
-            print("No elements in queue")
-            return
-        return self.queue[0]
-a=Queue()
-a.enqueue(1)
-a.enqueue(2)
-a.enqueue(3)
-a.dequeue()
-print(a.peek())
+            print("Queue is empty")
+        else:
+            return self.head.data 
+b=Queue()
+b.enqueue(100)
+b.enqueue(200)
+b.enqueue(300)
+b.dequeue()
+print(b.peek())
+
